7f4745d65a2d60f36274a3dc03a5cefe13605e8d
Julien Nadeau Carriere <vedge@hypertriton.com>
2001-07-22 01:14:09 +0000

    a makefile for perl stuff
    git-svn-id: file:///home/dev/bsdbuild/trunk@30 086a213a-9b02-0410-a1af-a7f5267bcc5c

---
+++ vedge.perl.mk
@@ -0,0 +1,41 @@
+# $Id$
+
+TYPE=	    perl
+
+PREFIX?=    /usr/local
+SH?=	    sh
+INSTALL?=   install -c
+PERLMODE?=   755
+
+.SUFFIXES:  .pl .pm .perl
+
+.pl.pl:
+	echo "===> $<"
+	perl $<
+
+ALL: $(OBJS) all-subdir
+
+clean: clean-subdir
+	@rm -f $(OBJS)
+
+depend: depend-subdir
+
+install: install-subdir
+	@if [ "$(OBJS)" != "" ]; then \
+	    for OBJ in $(OBJS); do \
+		echo "===> $$OBJ"; \
+		$(INSTALL) $(BINOWN) $(BINGRP) -m $(PERLMODE) \
+		    $$OBJ $(PREFIX); \
+	    done; \
+	fi
+	
+uninstall: uninstall-subdir
+	@if [ "$(OBJS)" != "" ]; then \
+	    for OBJ in $(OBJS); do \
+		echo "===> $$OBJ"; \
+		rm -f $(PREFIX)/$$OBJ; \
+	    done; \
+	fi
+
+include $(TOP)/mk/vedge.common.mk
+include $(TOP)/vedge.subdir.mk

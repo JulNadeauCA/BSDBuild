2414c8f122a9299d6902dfeac06fb9b01947abe5
Julien Nadeau Carriere <vedge@hypertriton.com>
2001-09-15 12:13:37 +0000

    hack.
    git-svn-id: file:///home/dev/bsdbuild/trunk@49 086a213a-9b02-0410-a1af-a7f5267bcc5c

--- hstrip.pl
+++ hstrip.pl
@@ -1,17 +1,21 @@
-# $Csoft: hstrip.pl,v 1.3 2001/09/15 04:11:51 vedge Exp $
+# $Csoft: hstrip.pl,v 1.4 2001/09/15 12:07:36 vedge Exp $
 
-exit(0) if($ARGV[0] eq 'contract.html');
+$no++ if($ARGV[0] eq 'contract.html');
 
 my $pre = 0;
 while (<STDIN>) {
-	$pre++ if (/<pre>/);
-	$pre-- if (/<\/pre>/);
+	if ($no) {
+		print $_;
+	} else {
+		$pre++ if (/<pre>/);
+		$pre-- if (/<\/pre>/);
 
-	if (!$pre) {
-		s/<!--.+-->//g;
-		chop;
+		if (!$pre) {
+			s/<!--.+-->//g;
+			chop;
+		}
+
+		print;
+		print ' ';
 	}
-
-	print;
-	print ' ';
 }

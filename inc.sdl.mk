dadd271e1f415364069816f7cac313b36d90edbd
Julien Nadeau Carriere <vedge@hypertriton.com>
2001-10-30 07:15:02 +0000

    add a crude check for existence.
    git-svn-id: file:///home/dev/bsdbuild/trunk@54 086a213a-9b02-0410-a1af-a7f5267bcc5c

--- inc.sdl.mk
+++ inc.sdl.mk
@@ -1,8 +1,17 @@
-# $Csoft$
+# $Csoft: inc.sdl.mk,v 1.1 2001/08/16 05:52:57 vedge Exp $
 
-SDLCFLAGS!= sdl-config --cflags
-SDLLIBS!=   sdl-config --libs
+SDLCFLAGS!=	sdl-config --cflags
+SDLLIBS!=	sdl-config --libs
+SDLVER!=	sdl-config --version
 
-CFLAGS+=    ${SDLCFLAGS}
-LIBS+=	    ${SDLLIBS}
+CFLAGS+=	${SDLCFLAGS}
+LIBS+=		${SDLLIBS}
+
+.BEGIN: inc-sdl-begin
+
+inc-sdl-begin:
+	@if [ "`sdl-config --version 2>/dev/null`" == "" ]; then \
+		echo "SDL is missing. Get it from http://www.libsdl.org/"; \
+		exit 1; \
+	fi
 

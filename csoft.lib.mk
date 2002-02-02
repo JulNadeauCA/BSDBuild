37fab108a8924c25e45570e6a2511251844e7e9b
Julien Nadeau Carriere <vedge@hypertriton.com>
2002-02-02 08:43:31 +0000

    - check for ranlib, it may be absent.
    git-svn-id: file:///home/dev/bsdbuild/trunk@112 086a213a-9b02-0410-a1af-a7f5267bcc5c

--- csoft.lib.mk
+++ csoft.lib.mk
@@ -1,4 +1,4 @@
-# $Csoft: csoft.lib.mk,v 1.10 2002/01/27 12:33:20 vedge Exp $
+# $Csoft: csoft.lib.mk,v 1.11 2002/02/01 03:37:05 vedge Exp $
 
 # Copyright (c) 2001 CubeSoft Communications, Inc.
 # <http://www.csoft.org>
@@ -127,8 +127,10 @@ lib${LIB}.a:	${OBJS}
 	@if [ "${LIB}" != "" ]; then \
 		echo "${AR} -cru lib${LIB}.a ${OBJS}"; \
 		${AR} -cru lib${LIB}.a ${OBJS}; \
-		echo "${RANLIB} lib${LIB}.a"; \
-		${RANLIB} lib${LIB}.a; \
+		if [ -x "`which ${RANLIB} 2>/dev/null`" ]; then \
+		    echo "${RANLIB} lib${LIB}.a"; \
+		    ${RANLIB} lib${LIB}.a; \
+		fi \
 	fi
 
 lib${LIB}.la:	${LIBTOOL} ${SHOBJS}

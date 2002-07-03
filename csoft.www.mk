9d059130387a14f6d4c2aceefa1d897eced6aa54
Julien Nadeau Carriere <vedge@hypertriton.com>
2002-07-03 04:14:29 +0000

    don't hstrip
    git-svn-id: file:///home/dev/bsdbuild/trunk@163 086a213a-9b02-0410-a1af-a7f5267bcc5c

--- csoft.www.mk
+++ csoft.www.mk
@@ -1,4 +1,4 @@
-# $Csoft: csoft.www.mk,v 1.7 2002/04/24 06:55:51 vedge Exp $
+# $Csoft: csoft.www.mk,v 1.8 2002/05/10 22:41:35 vedge Exp $
 
 # Copyright (c) 2001, 2002 CubeSoft Communications, Inc.
 # <http://www.csoft.org>
@@ -43,11 +43,11 @@ DEFAULT_TMPL?=	text
 	@for F in ${TEMPLATES}; do \
 	    echo "${M4} ${M4FLAGS} -D_TMPL_=$$F	\
 		-D_TOP_=${TOP} -D_BASE_=${BASEDIR} -D_FILE_=$@ \
-		${BASEDIR}/$$F.m4 | ${PERL} ${TOP}/mk/hstrip.pl $@"; \
+		${BASEDIR}/$$F.m4 $@"; \
 	    cp -f $< ${BASEDIR}/base.htm; \
 	    ${M4} ${M4FLAGS} -D_TMPL_=$$F \
 		-D_TOP_=${TOP} -D_BASE_=${BASEDIR} -D_FILE_=$@ \
-		${BASEDIR}/$$F.m4 | ${PERL} ${TOP}/mk/hstrip.pl $@ \
+		${BASEDIR}/$$F.m4 $@ \
 		> $$F-$@; \
 	done
 	@cp -f ${DEFAULT_TMPL}-$@ $@

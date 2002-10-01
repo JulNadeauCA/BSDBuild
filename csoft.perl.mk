49714e8bc3176c735bc4af8a044f2154ccccaa00
Julien Nadeau Carriere <vedge@hypertriton.com>
2002-10-01 08:37:44 +0000

    fantastik strapagosse
    git-svn-id: file:///home/dev/bsdbuild/trunk@200 086a213a-9b02-0410-a1af-a7f5267bcc5c

--- csoft.perl.mk
+++ csoft.perl.mk
@@ -1,4 +1,4 @@
-# $Csoft: csoft.perl.mk,v 1.9 2002/05/10 22:41:48 vedge Exp $
+# $Csoft: csoft.perl.mk,v 1.10 2002/09/06 00:58:47 vedge Exp $
 
 # Copyright (c) 2001, 2002 CubeSoft Communications, Inc. <http://www.csoft.org>
 # All rights reserved.
@@ -25,23 +25,94 @@
 
 .SUFFIXES:  .pl .pm
 
-PERL?=
+PERL?=		/usr/bin/perl
+
+SCRIPTS?=
+MODULES?=
+SHARE?=
+
+SCRIPTS_DIR?=	${INST_BINDIR}
+MODULES_DIR?=	${SHAREDIR}/perl
+
+SCRIPTS_SUBST?=
+MODULES_SUBST?=
 
 all: all-subdir
 
 install: install-subdir
-	@for F in ${PERL}; do \
-	    echo "${INSTALL_PROG} $$F ${INST_BINDIR}"; \
-	    ${INSTALL_PROG} $$F ${INST_BINDIR}; \
-	done
-	
+	if [ "${SCRIPTS}" != "" ]; then \
+	    if [ ! -d "${SCRIPTS_DIR}" ]; then \
+	        echo "${INSTALL_PROG_DIR} ${SCRIPTS_DIR}"; \
+	        ${INSTALL_PROG_DIR} ${SCRIPTS_DIR}; \
+	    fi; \
+	    if [ "${SCRIPTS_SUBST}" != "" ]; then \
+		    for F in ${SCRIPTS}; do \
+		        echo "sed -e '${SCRIPTS_SUBST}' $$F > ${SCRIPTS_DIR}/$$F"; \
+		        sed -e '${SCRIPTS_SUBST}' $$F > ${SCRIPTS_DIR}/$$F; \
+			chmod 555 ${SCRIPTS_DIR}/$$F; \
+		    done; \
+	    else \
+		for F in ${SCRIPTS}; do \
+		    echo "${INSTALL_PROG} $$F ${SCRIPTS_DIR}"; \
+		    ${INSTALL_PROG} $$F ${SCRIPTS_DIR}; \
+		done; \
+	    fi; \
+	fi
+	@if [ "${MODULES}" != "" ]; then \
+	    if [ ! -d "${MODULES_DIR}" ]; then \
+	        echo "${INSTALL_DATA_DIR} ${MODULES_DIR}"; \
+	        ${INSTALL_DATA_DIR} ${MODULES_DIR}; \
+	    fi; \
+	    if [ "${MODULES_SUBST}" != "" ]; then \
+		    for F in ${MODULES}; do \
+		        echo "sed -e '${MODULES_SUBST}' $$F > ${MODULES_DIR}/$$F"; \
+		        sed -e '${MODULES_SUBST}' $$F > ${MODULES_DIR}/$$F; \
+			chmod 444 ${SCRIPTS_DIR}/$$F; \
+		    done; \
+	    else \
+	        for F in ${MODULES}; do \
+	            echo "${INSTALL_DATA} $$F ${MODULES_DIR}"; \
+	            ${INSTALL_DATA} $$F ${MODULES_DIR}; \
+	        done; \
+	    fi; \
+	fi
+	@export _share="${SHARE}"; \
+        if [ "$$_share" != "" ]; then \
+            if [ ! -d "${SHAREDIR}" ]; then \
+                echo "${INSTALL_DATA_DIR} ${SHAREDIR}"; \
+                ${INSTALL_DATA_DIR} ${SHAREDIR}; \
+            fi; \
+            for F in $$_share; do \
+                echo "${INSTALL_DATA} $$F ${SHAREDIR}"; \
+                ${INSTALL_DATA} $$F ${SHAREDIR}; \
+            done; \
+	fi
+
 deinstall: deinstall-subdir
-	@for F in ${PERL}; do \
-	    echo "${DEINSTALL_PROG} ${INST_BINDIR}/$$F"; \
-	    ${DEINSTALL_PROG} ${INST_BINDIR}/$$F; \
-	done
+	@if [ "${SCRIPTS}" != "" ]; then \
+	    for F in ${SCRIPTS}; do \
+	        echo "${DEINSTALL_PROG} ${SCRIPTS_DIR}/$$F"; \
+	        ${DEINSTALL_PROG} ${SCRIPTS_DIR}/$$F; \
+	    done; \
+	fi
+	@if [ "${MODULES}" != "" ]; then \
+	    for F in ${MODULES}; do \
+	        echo "${DEINSTALL_DATA} ${MODULES_DIR}/$$F"; \
+	        ${DEINSTALL_DATA} ${MODULES_DIR}/$$F; \
+	    done; \
+	fi
+	@if [ "${SHARE}" != "" ]; then \
+	    for F in ${SHARE}; do \
+	        echo "${DEINSTALL_DATA} ${SHAREDIR}/$$F"; \
+	        ${DEINSTALL_DATA} ${SHAREDIR}/$$F; \
+	    done; \
+	fi
 
 clean: clean-subdir
+	@if [ "${CLEANFILES}" != "" ]; then \
+	    echo "rm -f ${CLEANFILES}"; \
+	    rm -f ${CLEANFILES}; \
+	fi
 
 depend: depend-subdir
 

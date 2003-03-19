555d95ab71010774b3d473e8d2e4a8fdb07a7c19
Julien Nadeau Carriere <vedge@hypertriton.com>
2003-03-19 05:14:43 +0000

    libjpeg test.
    git-svn-id: file:///home/dev/bsdbuild/trunk@263 086a213a-9b02-0410-a1af-a7f5267bcc5c

---
+++ Manuconf/jpeg.pm
@@ -0,0 +1,72 @@
+# $Csoft: smpeg.pm,v 1.8 2002/12/24 08:59:39 vedge Exp $
+# vim:ts=4
+#
+# Copyright (c) 2002 CubeSoft Communications, Inc. <http://www.csoft.org>
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistribution of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Neither the name of CubeSoft Communications, nor the names of its
+#    contributors may be used to endorse or promote products derived from
+#    this software without specific prior written permission.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE FOR
+# ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+# USE OF THIS SOFTWARE EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+sub Test
+{
+	my ($ver) = @_;
+
+	print Define('JPEG_CFLAGS', '-I/usr/local/include');
+	print Define('JPEG_LIBS', '"-L/usr/local/lib -ljpeg"');
+	print Echo("ok");
+	
+	print NEcho('checking whether libjpeg works...');
+	TryLibCompile 'HAVE_JPEG',
+	    '${JPEG_CFLAGS}', '${JPEG_LIBS}', << 'EOF';
+#include <stdio.h>
+#include <jpeglib.h>
+
+struct jpeg_error_mgr		jerr;
+struct jpeg_compress_struct	jcomp;
+
+int
+main(int argc, char *argv[])
+{
+	jcomp.err = jpeg_std_error(&jerr);
+
+	jpeg_create_compress(&jcomp);
+	jcomp.image_width = 32;
+	jcomp.image_height = 32;
+	jcomp.input_components = 3;
+	jcomp.in_color_space = JCS_RGB;
+
+	jpeg_set_defaults(&jcomp);
+	jpeg_set_quality(&jcomp, 75, TRUE);
+
+	jpeg_destroy_compress(&jcomp);
+	return (0);
+}
+EOF
+	return (0);
+}
+
+BEGIN
+{
+	$HOMEPAGE = 'ftp://ftp.uu.net/graphics/jpeg/';
+	$DESCR{'jpeg'} = "libjpeg ($HOMEPAGE)";
+	$TESTS{'jpeg'} = \&Test;
+}
+
+;1

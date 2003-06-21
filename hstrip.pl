5d23562bf9c18ee3db1e37d6d7c47b0a0c723c73
Julien Nadeau Carriere <vedge@hypertriton.com>
2003-06-21 21:19:18 +0000

    rien
    git-svn-id: file:///home/dev/bsdbuild/trunk@284 086a213a-9b02-0410-a1af-a7f5267bcc5c

--- hstrip.pl
+++ hstrip.pl
@@ -1,21 +1,9 @@
 # $Csoft: hstrip.pl,v 1.1 2002/12/02 07:07:31 vedge Exp $
 
-$no++; #if($ARGV[0] eq 'contract.html'); # XXX
-
-my $pre = 0;
 while (<STDIN>) {
-	if ($no) {
-		print $_;
-	} else {
-		$pre++ if (/<pre>/);
-		$pre-- if (/<\/pre>/);
-
-		if (!$pre) {
-			s/<!--.+-->//g;
-			chop;
-		}
-
-		print;
-		print ' ';
+	chop;
+	if (/^$/) {
+		next;
 	}
+	print $_, "\n";
 }

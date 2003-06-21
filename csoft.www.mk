15e6fa1dbcad2bc88b9389e8c477461b6a49d6a7
Julien Nadeau Carriere <vedge@hypertriton.com>
2003-06-21 21:17:09 +0000

    - remove multiple templates. - use the ml xsl transform for m17n.
    git-svn-id: file:///home/dev/bsdbuild/trunk@283 086a213a-9b02-0410-a1af-a7f5267bcc5c

--- csoft.www.mk
+++ csoft.www.mk
@@ -1,15 +1,14 @@
-# $Csoft: csoft.www.mk,v 1.11 2002/09/06 00:58:47 vedge Exp $
+# $Csoft: csoft.www.mk,v 1.1 2002/12/02 07:07:31 vedge Exp $
 
 # Copyright (c) 2001, 2002, 2003 CubeSoft Communications, Inc.
 # <http://www.csoft.org>
-# All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
 # are met:
-# 1. Redistributions of source code must retain the above copyright
+# 1. Redistribution of source code must retain the above copyright
 #    notice, this list of conditions and the following disclaimer.
-# 2. Redistributions in binary form must reproduce the above copyright
+# 2. Redistribution in binary form must reproduce the above copyright
 #    notice, this list of conditions and the following disclaimer in the
 #    documentation and/or other materials provided with the distribution.
 # 
@@ -24,38 +23,41 @@
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
 # USE OF THIS SOFTWARE EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-include ${TOP}/Makefile.inc
-
 DOCROOT?=	./docroot
 M4?=		m4
+XSLTPROC?=	xsltproc
 PERL?=		perl
-M4FLAGS?=
 
 BASEDIR?=	${TOP}/base
-TEMPLATES?=	text
-DEFAULT_TMPL?=	text
+TEMPLATE?=	black
+LANGUAGES?=	en fr
+MLXSL?=		${TOP}/xsl/ml.xsl
 
 .SUFFIXES: .html .htm .jpg .jpeg .png .gif .m4
 
 .htm.html:
-	@for F in ${TEMPLATES}; do \
-	    echo "${M4} ${M4FLAGS} -D_TMPL_=$$F	\
-		-D_TOP_=${TOP} -D_BASE_=${BASEDIR} -D_FILE_=$@ \
-		${BASEDIR}/$$F.m4"; \
-	    cp -f $< ${BASEDIR}/base.htm; \
-	    ${M4} ${M4FLAGS} -D_TMPL_=$$F \
-		-D_TOP_=${TOP} -D_BASE_=${BASEDIR} -D_FILE_=$@ \
-		${BASEDIR}/$$F.m4 > $$F-$@; \
+	@cp -f $< ${BASEDIR}/base.htm
+	@echo -n "$@:"
+	@for LANG in ${LANGUAGES}; do \
+	    echo -n " $$LANG"; \
+	    ${M4} -D__BASE_DIR=${BASEDIR} -D__FILE=$@ -D__LANG=$$LANG \
+	        ${BASEDIR}/${TEMPLATE}.m4 \
+		| ${PERL} ${TOP}/mk/hstrip.pl > $@.$$LANG.prep; \
+            ${XSLTPROC} --html --nonet --stringparam lang $$LANG ${MLXSL} \
+	        $@.$$LANG.prep > $@.$$LANG 2>/dev/null; \
+	    rm -f $@.$$LANG.prep; \
 	done
-	@cp -f ${DEFAULT_TMPL}-$@ $@
+	@echo "."
 
 all: ${HTML} all-subdir
 
 clean: clean-subdir
-	@if [ "${HTML}" != "" ]; then \
-	    echo "rm -f *.html"; \
-	    rm -f *.html; \
-	fi
+	@for F in ${HTML}; do \
+		rm -f $$F; \
+		for LANG in ${LANGUAGES}; do \
+			rm -f $$F.$$LANG; \
+		done; \
+	done
 
 cleandir: cleandir-subdir
 	rm -f *~
@@ -70,13 +72,13 @@ install: install-subdir ${HTML}
 	
 deinstall: deinstall-subdir
 	@for F in ${HTML}; do \
-	    echo "rm -f ${DOCROOT}/$$F"; \
-	    rm -f ${DOCROOT}/$$F; \
+	    echo "rm -f ${DOCROOT}/${TEMPLATE}"; \
+	    rm -f ${DOCROOT}/${TEMPLATE}; \
 	done
 
 regress: regress-subdir
 
-.PHONY: clean cleandir depend install deinstall regress
-
 include ${TOP}/mk/csoft.common.mk
 include ${TOP}/mk/csoft.subdir.mk
+
+.PHONY: clean cleandir depend install deinstall regress

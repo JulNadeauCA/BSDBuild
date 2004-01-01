79272a11eb1a8fe34a1c9ba513cb632ea46c01a9
Julien Nadeau Carriere <vedge@hypertriton.com>
2004-01-01 00:33:41 +0000

    check for arc4random()
    git-svn-id: file:///home/dev/bsdbuild/trunk@361 086a213a-9b02-0410-a1af-a7f5267bcc5c

---
+++ Manuconf/arc4random.pm
@@ -0,0 +1,50 @@
+# $Csoft: strlcpy.pm,v 1.3 2003/10/01 09:24:19 vedge Exp $
+# vim:ts=4
+#
+# Copyright (c) 2003 CubeSoft Communications, Inc.
+# <http://www.csoft.org>
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE FOR
+# ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+# USE OF THIS SOFTWARE EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+sub Test
+{
+	TryCompile 'HAVE_ARC4RANDOM', << 'EOF';
+#include <sys/types.h>
+#include <stdlib.h>
+int
+main(int argc, char *argv[])
+{
+	u_int32_t i;
+
+	i = arc4random();
+	return (0);
+}
+EOF
+}
+
+BEGIN
+{
+	$TESTS{'arc4random'} = \&Test;
+	$DESCR{'arc4random'} = 'the arc4random() random number generator';
+}
+
+;1

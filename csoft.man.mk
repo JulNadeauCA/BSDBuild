d15433ed77895040e50da0a8dbd46143000588d8
Julien Nadeau Carriere <vedge@hypertriton.com>
2004-02-26 07:42:56 +0000

    what was i smoking?
    git-svn-id: file:///home/dev/bsdbuild/trunk@371 086a213a-9b02-0410-a1af-a7f5267bcc5c

--- csoft.man.mk
+++ csoft.man.mk
@@ -1,4 +1,4 @@
-# $Csoft: csoft.man.mk,v 1.32 2003/12/10 02:29:30 vedge Exp $
+# $Csoft: csoft.man.mk,v 1.33 2004/01/03 04:13:27 vedge Exp $
 
 # Copyright (c) 2001, 2002, 2003, 2004 CubeSoft Communications, Inc.
 # <http://www.csoft.org>
@@ -52,7 +52,7 @@ depend: depend-subdir
 	  sed 's,\$$SYSCONFDIR,${SYSCONFDIR},' | \
 	  sed 's,\$$PREFIX,${PREFIX},' | \
 	  sed 's,\$$SHAREDIR,${SHAREDIR},' | \
-	  ${NROFF} -Tascii -mandoc > $@) || (rm -f $@; false)
+	  ${NROFF} -Tascii -mandoc > $@) || (rm -f $@; true)
 
 .1.ps1 .2.ps2 .3.ps3 .4.ps4 .5.ps5 .6.ps6 .7.ps7 .8.ps8 .9.ps9:
 	@echo "${NROFF} -Tps -mandoc $< > $@"
@@ -60,7 +60,7 @@ depend: depend-subdir
 	  sed 's,\$$SYSCONFDIR,${SYSCONFDIR},' | \
 	  sed 's,\$$PREFIX,${PREFIX},' | \
 	  sed 's,\$$SHAREDIR,${SHAREDIR},' | \
-	  ${NROFF} -Tps -mandoc > $@) || (rm -f $@; false)
+	  ${NROFF} -Tps -mandoc > $@) || (rm -f $@; true)
 
 preformat-man:
 	@if [ "${MAN1}" != "" ]; then \

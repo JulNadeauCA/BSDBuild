fc568c70e1be5af7704916f908262f15fd168a97
Julien Nadeau Carriere <vedge@hypertriton.com>
2004-03-13 09:26:31 +0000

    err
    git-svn-id: file:///home/dev/bsdbuild/trunk@380 086a213a-9b02-0410-a1af-a7f5267bcc5c

--- csoft.lib.mk
+++ csoft.lib.mk
@@ -1,4 +1,4 @@
-# $Csoft: csoft.lib.mk,v 1.39 2004/01/03 04:13:27 vedge Exp $
+# $Csoft: csoft.lib.mk,v 1.40 2004/03/10 13:46:17 vedge Exp $
 
 # Copyright (c) 2001, 2002, 2003, 2004 CubeSoft Communications, Inc.
 # <http://www.csoft.org>
@@ -234,8 +234,8 @@ clean-lib:
 		    rm -f ${OBJS}; \
 		    echo "rm -f ${OBJS}"; \
 		fi; \
-		echo "rm -f lib${LIB}.la .libs"; \
-		rm -f lib${LIB}.la .libs; \
+		echo "rm -fR lib${LIB}.la .libs"; \
+		rm -fR lib${LIB}.la .libs; \
 	    fi; \
 	fi
 	@if [ "${CLEANFILES}" != "" ]; then \

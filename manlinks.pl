365fb5f5ad54a9624488bf480b54791f21c19b25
Julien Nadeau Carriere <vedge@hypertriton.com>
2006-10-05 01:28:32 +0000

    apply MANLINKS to preformatted files as well.
    git-svn-id: file:///home/dev/bsdbuild/trunk@506 086a213a-9b02-0410-a1af-a7f5267bcc5c

--- manlinks.pl
+++ manlinks.pl
@@ -5,21 +5,24 @@
 #
 
 my $man = $ARGV[0];
-my $section = $ARGV[1];
 my $ns = 0;
 
-unless ($man && $section) {
-	print STDERR "Usage: $0 [man] [section]\n";
+unless ($man) {
+	print STDERR "Usage: $0 [man]\n";
 	exit 1;
 }
 
-$man =~ s/.+\/([\w\-\.]+)$/$1/;
+$man =~ s/([\w\-]+)\.(\d)$/$1/;
+$section = $2;
+print STDERR "Creating links to $man($section)\n";
 
 while (<STDIN>) {
 	if (/^\.nr nS 1/) { $ns = 1; }
 	elsif (/^\.nr nS 0/ || /^\.\\" NOMANLINK/) { $ns = 0; }
 	next unless $ns;
-	if (/^\.Fn ([\w\-]+)\s+/ && $1.'.'.$section ne $man) {
-		print "MANLINKS+=$man:$1.$section\n";
+	if (/^\.Fn ([\w\-]+)\s+/ &&
+	    $1.'.'.$section ne $man.'.'.$section) {
+		print "MANLINKS+=$man.${section}:$1.${section}\n";
+		print "CATLINKS+=$man.cat${section}:$1.cat${section}\n";
 	}
 }

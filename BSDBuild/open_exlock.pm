16b3fd44113c7fed8c68c563a3320cd63a90cd8d
Julien Nadeau Carriere <vedge@hypertriton.com>
2007-10-29 09:10:01 +0000

    + test for O_EXLOCK flag + test for fgetln()
    git-svn-id: file:///home/dev/bsdbuild/trunk@619 086a213a-9b02-0410-a1af-a7f5267bcc5c

---
+++ BSDBuild/open_exlock.pm
@@ -0,0 +1,52 @@
+# vim:ts=4
+#
+# Copyright (c) 2007 CubeSoft Communications, Inc.
+# <http://www.csoft.org>
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE FOR
+# ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+# USE OF THIS SOFTWARE EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+sub Test
+{
+	TryCompile 'HAVE_OPEN_EXLOCK', << 'EOF';
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+int
+main(int argc, char *argv[])
+{
+	int fd;
+
+	fd = open("foo", O_WRONLY|O_CREAT|O_EXCL);
+	close(fd);
+	return (0);
+}
+EOF
+}
+
+BEGIN
+{
+	$TESTS{'open_exlock'} = \&Test;
+	$DESCR{'open_exlock'} = 'the O_EXLOCK open() flag';
+}
+
+;1

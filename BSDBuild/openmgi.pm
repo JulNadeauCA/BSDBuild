7b39e301fe464c1997bfcb54025512aab50c5979
Julien Nadeau Carriere <vedge@hypertriton.com>
2008-02-18 04:01:55 +0000

    tri:+openmgi
    git-svn-id: file:///home/dev/bsdbuild/trunk@678 086a213a-9b02-0410-a1af-a7f5267bcc5c

---
+++ BSDBuild/openmgi.pm
@@ -0,0 +1,64 @@
+# vim:ts=4
+#
+# Copyright (c) 2008 Hypertriton, Inc. <http://hypertriton.com/>
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE FOR
+# ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+# USE OF THIS SOFTWARE EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+sub Test
+{
+	my ($ver) = @_;
+	
+	MkExecOutput('openmgi-config', '--version', 'OPENMGI_VERSION');
+	MkExecOutput('openmgi-config', '--cflags', 'OPENMGI_CFLAGS');
+	MkExecOutput('openmgi-config', '--libs', 'OPENMGI_LIBS');
+
+	MkIf('"${OPENMGI_VERSION}" != ""');
+		MkPrint('yes');
+		MkPrintN('checking whether libopenmgi works...');
+		MkCompileC('HAVE_OPENMGI', '${OPENMGI_CFLAGS}', '${OPENMGI_LIBS}',
+	               << 'EOF');
+#include <openmgi/openmgi.h>
+int main(int argc, char *argv[]) {
+	int rv;
+	rv = MGI_Init(0);
+	MGI_Destroy();
+	return (0);
+}
+EOF
+		MkIf('"${HAVE_OPENMGI}" != "no"');
+			MkSaveDefine('OPENMGI_CFLAGS', 'OPENMGI_LIBS');
+			MkSaveMK	('OPENMGI_CFLAGS', 'OPENMGI_LIBS');
+		MkEndif;
+	MkElse;
+		MkPrint('no');
+		MkSaveUndef('HAVE_OPENMGI');
+	MkEndif;
+	return (0);
+}
+
+BEGIN
+{
+	$DESCR{'openmgi'} = 'OpenMGI (http://www.openmgi.org/)';
+	$DEPS{'openmgi'} = 'cc';
+	$TESTS{'openmgi'} = \&Test;
+}
+;1

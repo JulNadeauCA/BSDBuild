38f8932ed4408f3d1299435721e87dcfc70e4ad0
Julien Nadeau Carriere <vedge@hypertriton.com>
2008-06-27 05:20:44 +0000

    test for libcurl
    git-svn-id: file:///home/dev/bsdbuild/trunk@713 086a213a-9b02-0410-a1af-a7f5267bcc5c

---
+++ BSDBuild/curl.pm
@@ -0,0 +1,67 @@
+# vim:ts=4
+#
+# Copyright (c) 2008 Hypertriton, Inc. <http://hypertriton.com/>
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE FOR
+# ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+# USE OF THIS SOFTWARE EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+sub Test
+{
+	my ($ver) = @_;
+	
+	MkExecOutput('curl-config', '--version', 'CURL_VERSION');
+
+	MkIf('"${CURL_VERSION}" != ""');
+		MkPrint('yes');
+		MkExecOutput('curl-config', '--cflags', 'CURL_CFLAGS');
+		MkExecOutput('curl-config', '--libs', 'CURL_LIBS');
+        MkSaveMK('CURL_CFLAGS', 'CURL_LIBS');
+        MkSaveDefine('CURL_CFLAGS', 'CURL_LIBS');
+
+		MkPrintN('checking whether libcurl works...');
+		MkCompileC('HAVE_CURL', '${CURL_CFLAGS}', '${CURL_LIBS}', << 'EOF');
+#include <curl/curl.h>
+
+int
+main(int argc, char *argv[])
+{
+	curl_version_info_data *v;
+	curl_global_init(CURL_GLOBAL_ALL);
+	v = curl_version_info(CURLVERSION_NOW);
+	curl_global_cleanup();
+	return (0);
+}
+EOF
+	MkElse;
+		MkPrint('no');
+	    MkSaveUndef('HAVE_CURL', 'CURL_CFLAGS', 'CURL_LIBS');
+	MkEndif;
+	return (0);
+}
+
+BEGIN
+{
+	$TESTS{'curl'} = \&Test;
+	$DEPS{'curl'} = 'cc';
+	$DESCR{'curl'} = 'libcurl (http://curl.haxx.se/)';
+}
+
+;1

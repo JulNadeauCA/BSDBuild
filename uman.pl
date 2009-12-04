cb4689a390e2f8d1f316d0187d99e5154335403e
Julien Nadeau Carriere <vedge@hypertriton.com>
2009-12-04 06:24:25 +0000

    manpage reader for uninstalled manpage source; also looks up manlinks files.
    git-svn-id: file:///home/dev/bsdbuild/trunk@825 086a213a-9b02-0410-a1af-a7f5267bcc5c

---
+++ uman.pl
@@ -0,0 +1,97 @@
+#!/usr/bin/perl
+#
+# Public domain
+# 
+# uman.pl: Search source directories for an uninstalled manual page.
+# The script also looks up BSDBuild-generated .manlinks.mk files.
+#
+
+if (!defined($ENV{'UMANPATH'})) {
+	print "The \$UMANPATH environment variable is not set; see uman(1).\n";
+	exit(1);
+}
+
+# Search these directories for manual page source.
+@SRC_DIRS = split(':', $ENV{'UMANPATH'});
+(
+	$ENV{'HOME'}.'/src/agar',
+	$ENV{'HOME'}.'/src/freesg',
+	$ENV{'HOME'}.'/src/cadtools',
+	$ENV{'HOME'}.'/src/edacious'
+);
+
+# Formatting engine
+$NROFF = 'nroff -Tascii -mandoc';
+
+if (@ARGV < 1) { die "Usage: aman [manpage]"; }
+my $query = $ARGV[0];
+my $pager = 'less';
+if (exists($ENV{'PAGER'})) { $pager = $ENV{'PAGER'}; }
+
+sub ReadPage ($)
+{
+	my $page = shift;
+	system("cat $page |$NROFF $page |$pager");
+}
+
+# Search a BSDBuild-generated .manlinks.mk file.
+sub SearchManlinksMK ($$)
+{
+	my $path = shift;
+	my $q = shift;
+
+	unless (open(ML, $path)) {
+		print STDERR "$path: $!; ignoring\n";
+		return;
+	}
+	foreach $_ (<ML>) {
+		if (/^MANLINKS\+=([\w\-]+)\.(\d):([\w\-]+)\.(\d)$/) {
+			my $from = $1.'.'.$2;
+
+			if (lc($3) eq $q) {
+				return ($from);
+			}
+		}
+	}
+	close(ML);
+	return (undef);
+}
+
+# Recursively search a directory.
+sub SearchDir ($$$$)
+{
+	my $dir = shift;
+	my $q = lc(shift);
+	my $depth = shift;
+	my $maxDepth = shift;
+
+	unless (opendir(DIR, $dir)) {
+		return;
+	}
+	foreach my $ent (readdir(DIR)) {
+		my $path = $dir.'/'.$ent;
+		if ($ent eq '.manlinks.mk') {
+			if ((my $rv = SearchManlinksMK($path, $q))) {
+				ReadPage($dir.'/'.$rv);
+				exit(0);
+			}
+			next;
+		}
+		if ($ent =~ /^\./) { next; }
+		if ($ent =~ /^([\w\-]+)\.(\d)$/) {
+			if ($q eq lc($1) ||
+			    $q eq lc($1.'.'.$2)) {
+				ReadPage($path);
+				exit(0);
+			}
+		}
+		if (-d $path && $depth+1 <= $maxDepth) {
+			SearchDir($path, $q, $depth+1, $maxDepth);
+		}
+	}
+	closedir(DIR);
+}
+
+foreach my $dir (@SRC_DIRS) {
+	SearchDir($dir, $query, 1, 10);
+}

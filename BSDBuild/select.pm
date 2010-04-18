6cdaef658d567b5ae9882e402b1cf052eea1c90a
Julien Nadeau Carriere <vedge@hypertriton.com>
2010-04-18 13:27:06 +0000

    test for select()
    git-svn-id: file:///home/dev/bsdbuild/trunk@844 086a213a-9b02-0410-a1af-a7f5267bcc5c

---
+++ BSDBuild/select.pm
@@ -0,0 +1,70 @@
+# vim:ts=4
+#
+# Copyright (c) 2010 Hypertriton, Inc. <http://hypertriton.com/>
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE FOR
+# ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+# USE OF THIS SOFTWARE EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+sub Test
+{
+	TryCompile 'HAVE_SELECT', << 'EOF';
+#include <sys/types.h>
+#include <sys/time.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+
+int
+main(int argc, char *argv[])
+{
+	struct timeval tv;
+	int rv;
+
+	tv.tv_sec = 1;
+	tv.tv_usec = 1;
+	rv = select(0, NULL, NULL, NULL, &tv);
+	return (rv == -1 && errno != EINTR);
+}
+EOF
+}
+
+sub Emul
+{
+	my ($os, $osrel, $machine) = @_;
+
+	if ($os eq 'linux' || $os eq 'darwin' || $os =~ /^(open|net|free)bsd$/) {
+		MkDefine('HAVE_SELECT', 'yes');
+		MkSaveDefine('HAVE_SELECT');
+	} else {
+		MkSaveUndef('HAVE_SELECT');
+	}
+	return (1);
+}
+
+BEGIN
+{
+	$DESCR{'select'} = 'the select() interface';
+	$DEPS{'select'} = 'cc';
+	$TESTS{'select'} = \&Test;
+	$EMUL{'select'} = \&Emul;
+}
+
+;1

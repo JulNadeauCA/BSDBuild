33a491313fb67392837a8297761a49a7d887dc4b
Julien Nadeau Carriere <vedge@hypertriton.com>
2010-08-10 01:30:29 +0000

    test for execvp()
    git-svn-id: file:///home/dev/bsdbuild/trunk@871 086a213a-9b02-0410-a1af-a7f5267bcc5c

---
+++ BSDBuild/execvp.pm
@@ -0,0 +1,68 @@
+# $Csoft: gethostname.pm,v 1.2 2004/01/03 04:13:29 vedge Exp $
+# vim:ts=4
+#
+# Copyright (c) 2010 Hypertriton, Inc.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE FOR
+# ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+# USE OF THIS SOFTWARE EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+sub Test
+{
+	TryCompile 'HAVE_EXECVP', << 'EOF';
+#include <unistd.h>
+
+int
+main(int argc, char *argv[])
+{
+	char *args[3];
+	int rv;
+
+	args[0] = "foo";
+	args[2] = NULL;
+
+	rv = execvp(args[0], args);
+	return (rv);
+}
+EOF
+}
+
+sub Emul
+{
+	my ($os, $osrel, $machine) = @_;
+
+	if ($os eq 'linux' || $os eq 'darwin' || $os =~ /^(open|net|free)bsd$/) {
+		MkDefine('HAVE_EXECVP', 'yes');
+		MkSaveDefine('HAVE_EXECVP');
+	} else {
+		MkSaveUndef('HAVE_EXECVP');
+	}
+	return (1);
+}
+
+BEGIN
+{
+	$DESCR{'execvp'} = 'the execvp() function';
+	$TESTS{'execvp'} = \&Test;
+	$EMUL{'execvp'} = \&Emul;
+	$DEPS{'execvp'} = 'cc';
+}
+
+;1

fa876e397b82074b158d190720f768db1f3034ef
Julien Nadeau Carriere <vedge@hypertriton.com>
2010-10-18 01:02:43 +0000

    test for glib2; thanks rhaamo!
    git-svn-id: file:///home/dev/bsdbuild/trunk@887 086a213a-9b02-0410-a1af-a7f5267bcc5c

---
+++ BSDBuild/glib2.pm
@@ -0,0 +1,61 @@
+# vim:ts=4
+#
+# Copyright (c) 2002-2007 Hypertriton, Inc. <http://hypertriton.com/>
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE FOR
+# ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+# USE OF THIS SOFTWARE EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+my $testCode = << 'EOF';
+#include <glib-2.0/glib.h>
+int main(int argc, char *argv[]) {
+  void *slist = g_slist_alloc();
+  g_slist_free(slist);
+  return (0);
+}
+EOF
+
+sub Test
+{
+	my ($ver, $pfx) = @_;
+	
+	MkExecPkgConfig($pfx, 'glib-2.0', '--modversion', 'GLIB2_VERSION');
+	MkExecPkgConfig($pfx, 'glib-2.0', '--cflags', 'GLIB2_CFLAGS');
+	MkExecPkgConfig($pfx, 'glib-2.0', '--libs', 'GLIB2_LIBS');
+	MkIfNE('${GLIB2_VERSION}', '');
+		MkFoundVer($pfx, $ver, 'GLIB2_VERSION');
+		MkPrintN('checking whether glib 2.x works...');
+		MkCompileC('HAVE_GLIB2',
+			   '${GLIB2_CFLAGS}', '${GLIB2_LIBS}',
+			    $testCode);
+		MkSaveIfTrue('${HAVE_GLIB2}', 'GLIB2_CFLAGS', 'GLIB2_LIBS');
+	MkElse;
+		MkNotFound($pfx);
+		MkSaveUndef('HAVE_GLIB2');
+	MkEndif;
+	return (0);
+}
+
+BEGIN
+{
+	$DESCR{'glib2'} = 'Glib 2.x (http://www.gtk.org/)';
+	$TESTS{'glib2'} = \&Test;
+	$DEPS{'glib2'} = 'cc';
+}
+;1

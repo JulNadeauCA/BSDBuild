5e3e862900135779b76e2c0453f6629b6ac483e1
Julien Nadeau Carriere <vedge@hypertriton.com>
2011-04-20 10:40:27 +0000

    + agar-au
    git-svn-id: file:///home/dev/bsdbuild/trunk@897 086a213a-9b02-0410-a1af-a7f5267bcc5c

---
+++ BSDBuild/agar-au.pm
@@ -0,0 +1,104 @@
+# $Csoft: agar.pm,v 1.7 2005/09/27 00:29:42 vedge Exp $
+# vim:ts=4
+#
+# Copyright (c) 2011 Hypertriton, Inc. <http://hypertriton.com/>
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE FOR
+# ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+# USE OF THIS SOFTWARE EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+my $testCode = << 'EOF';
+#include <agar/core.h>
+#include <agar/au.h>
+
+int main(int argc, char *argv[]) {
+	AU_InitSubsystem();
+	AU_DestroySubsystem();
+	return (0);
+}
+EOF
+
+sub Test
+{
+	my ($ver, $pfx) = @_;
+	
+	MkExecOutputPfx($pfx, 'agar-au-config', '--version', 'AGAR_AU_VERSION');
+	MkIfNE('${AGAR_AU_VERSION}', '');
+		MkFoundVer($pfx, $ver, 'AGAR_AU_VERSION');
+		MkPrintN('checking whether agar-au works...');
+		MkExecOutputPfx($pfx, 'agar-config', '--cflags', 'AGAR_CFLAGS');
+		MkExecOutputPfx($pfx, 'agar-config', '--libs', 'AGAR_LIBS');
+		MkExecOutputPfx($pfx, 'agar-au-config', '--cflags', 'AGAR_AU_CFLAGS');
+		MkExecOutputPfx($pfx, 'agar-au-config', '--libs', 'AGAR_AU_LIBS');
+		MkCompileC('HAVE_AGAR_AU',
+		           '${AGAR_AU_CFLAGS} ${AGAR_CFLAGS}',
+		           '${AGAR_AU_LIBS} ${AGAR_LIBS}',
+				   $testCode);
+		MkSaveIfTrue('${HAVE_AGAR_AU}', 'AGAR_AU_CFLAGS', 'AGAR_AU_LIBS');
+	MkElse;
+		MkNotFound($pfx);
+		MkSaveUndef('HAVE_AGAR_AU', 'AGAR_AU_CFLAGS', 'AGAR_AU_LIBS');
+	MkEndif;
+	return (0);
+}
+
+sub Emul
+{
+	my ($os, $osrel, $machine) = @_;
+
+	if ($os eq 'windows') {
+		MkDefine('AGAR_AU_CFLAGS', '');
+		MkDefine('AGAR_AU_LIBS', 'ag_au ag_au_pa');
+	} else {
+		MkDefine('AGAR_AU_CFLAGS', '-I/usr/local/include/agar '.
+		                           '-I/usr/include/agar');
+		MkDefine('AGAR_AU_LIBS', '-L/usr/local/lib -lag_au -lag_au_pa');
+	}
+	MkDefine('HAVE_AGAR_AU', 'yes');
+	MkSave('HAVE_AGAR_AU', 'AGAR_AU_CFLAGS', 'AGAR_AU_LIBS');
+	return (1);
+}
+
+sub Link
+{
+	my $var = shift;
+
+	if ($var ne 'ag_au') {
+		return (0);
+	}
+	PmLink('ag_au');
+	PmLink('ag_au_pa');
+	if ($EmulEnv =~ /^cb-/) {
+		PmIncludePath('$(#agar.include)');
+		PmLibPath('$(#agar.lib)');
+	}
+	return (1);
+}
+
+BEGIN
+{
+	$DESCR{'agar-au'} = 'agar-au (http://libagar.org/)';
+	$DEPS{'agar-au'} = 'cc,agar';
+	$TESTS{'agar-au'} = \&Test;
+	$LINK{'agar-au'} = \&Link;
+	$EMUL{'agar-au'} = \&Emul;
+}
+
+;1

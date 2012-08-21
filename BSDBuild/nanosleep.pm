ac335ff08ffb0ee6bda3048d9cac3aacb11c69e5
Julien Nadeau Carriere <vedge@hypertriton.com>
2012-08-21 06:27:36 +0000

    test for nanosleep()
    git-svn-id: file:///home/dev/bsdbuild/trunk@996 086a213a-9b02-0410-a1af-a7f5267bcc5c

---
+++ BSDBuild/nanosleep.pm
@@ -0,0 +1,61 @@
+# vim:ts=4
+#
+# Copyright (c) 2012 Hypertriton, Inc. <http://hypertriton.com/>
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE FOR
+# ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+# USE OF THIS SOFTWARE EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+sub Test
+{
+	TryCompile 'HAVE_NANOSLEEP', << 'EOF';
+#include <time.h>
+
+int
+main(int argc, char *argv[])
+{
+	struct timespec rqtp, rmtp;
+	int rv;
+
+	rqtp.tv_sec = 1;
+	rqtp.tv_nsec = 1000000;
+	rv = nanosleep(&rqtp, &rmtp);
+	return (rv == -1);
+}
+EOF
+}
+
+sub Emul
+{
+	my ($os, $osrel, $machine) = @_;
+
+	MkEmulUnavailSYS('NANOSLEEP');
+	return (1);
+}
+
+BEGIN
+{
+	$TESTS{'nanosleep'} = \&Test;
+	$DEPS{'nanosleep'} = 'cc';
+	$EMUL{'nanosleep'} = \&Emul;
+	$DESCR{'nanosleep'} = 'the nanosleep() function';
+}
+
+;1

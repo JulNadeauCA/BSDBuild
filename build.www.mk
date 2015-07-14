23a1a5fd28dc9914d9dbdc8b62d63b6ee967b713
Julien Nadeau Carriere <vedge@hypertriton.com>
2015-07-14 06:52:00 +0000

    - clean up the variant generation code; the .html.var files are now used   as primary targets. add ${CHARSETS} option. - move source install code to separate targets: install-www-{makefile,source,base}
    git-svn-id: file:///home/dev/bsdbuild/trunk@1044 086a213a-9b02-0410-a1af-a7f5267bcc5c

--- build.www.mk
+++ build.www.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (c) 2001-2010 Hypertriton, Inc. <http://hypertriton.com/>
+# Copyright (c) 2001-2015 Hypertriton, Inc. <http://hypertriton.com/>
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
@@ -29,23 +29,28 @@
 M4?=		m4
 M4FLAGS?=
 XSLTPROC?=	xsltproc
+XSLTPROCFLAGS?=	--nonet
 PERL?=		perl
 ICONV?=		iconv
 BASEDIR?=	${TOP}/m4
 XSLDIR?=	${TOP}/xsl
 TEMPLATE?=	simple
+TEMPLATE_DEPS?=
 LANGUAGES?=	en fr
+CHARSETS?=	utf8 iso8859-1
 DEF_LANGUAGE?=	en
 XSL?=		${XSLDIR}/ml.xsl
 MKDEPS=		build.www.mk build.subdir.mk build.common.mk hstrip.pl
 CLEANFILES?=
 HTMLDIR?=	none
 HTML?=
-HTML_DEPS?=
+HTML_EXTRA?=
 CSS?=
-CSS_TEMPLATE?=	style
-HTML_OVERWRITE?=	No
-HTML_INSTSOURCE?=	Yes
+CSS_TEMPLATE?=style
+CSS_TEMPLATE_DEPS?=
+HTML_OVERWRITE?=No
+HTML_INSTSOURCE?=Yes
+HTML_STRIP?=${PERL} ${TOP}/mk/hstrip.pl
 
 DTD?=		<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"  \
 		"http://www.w3.org/TR/html4/loose.dtd">
@@ -58,189 +63,235 @@ deinstall: deinstall-subdir
 regress: regress-subdir
 depend: depend-subdir
 
-.SUFFIXES: .html .htm .jpg .jpeg .png .gif .m4 .css .css-in
+.SUFFIXES: .html.var .html .htm .jpg .jpeg .png .gif .m4 .css .css-in
 
-.css-in.css:
+.css-in.css: ${BASEDIR}/${CSS_TEMPLATE}.m4 ${CSS_TEMPLATE_DEPS}
 	@cp -f $< ${BASEDIR}/base.css
 	@echo -n "$@:"
 	${M4} ${M4FLAGS} -D__BASE_DIR=${BASEDIR} -D__FILE=$@ \
-	    -D__LANG=$$LANG \
-	    ${BASEDIR}/${CSS_TEMPLATE}.m4 \
-	    | ${PERL} ${TOP}/mk/hstrip.pl > $@
+	    -D__LANG=$$LANG ${BASEDIR}/${CSS_TEMPLATE}.m4 | ${HTML_STRIP} > $@
 	@rm -f ${BASEDIR}/base.css
 
-.htm.html:
+.htm.html: ${BASEDIR}/${TEMPLATE}.m4 ${TEMPLATE_DEPS}
+	@if [ ! -e "utf8" ]; then mkdir utf8; fi
 	@cp -f $< ${BASEDIR}/base.htm
-	@echo -n "$@:"
-	@echo > $@.var
-	@for LANG in ${LANGUAGES}; do \
+	@export OUT=".$@.tmp"
+	${M4} ${M4FLAGS} -D__BASE_DIR=${BASEDIR} -D__FILE=$@ \
+	    -D__TEMPLATE=${TEMPLATE} -D__LANG=${DEF_LANGUAGE} \
+	        ${BASEDIR}/${TEMPLATE}.m4 | \
+		${HTML_STRIP} > "$$OUT"; \
+	@echo '${DTD}' > $@
+	${XSLTPROC} ${XSLTPROCFLAGS} --html --stringparam lang ${DEF_LANGUAGE} ${XSL} \
+	    "$$OUT" 2>/dev/null | ${HTML_STRIP} >> $@ 2>/dev/null
+	@cp "$@" "utf8/$@"
+	@rm -f "$$OUT" ${BASEDIR}/base.htm
+
+.htm.html.var: ${BASEDIR}/${TEMPLATE}.m4 ${TEMPLATE_DEPS}
+	@for CHARSET in ${CHARSETS}; do \
+	    if [ ! -e "$$CHARSET" ]; then mkdir $$CHARSET; fi
+	done
+	@cp -f $< ${BASEDIR}/base.htm
+	@export BASE="`echo $@ | sed s/\.var//`"; \
+	echo -n "$$BASE:"; \
+	echo > $@; \
+	for LANG in ${LANGUAGES}; do \
+	    export OUT=".$BASE.$$LANG.tmp"; \
 	    echo -n " $$LANG"; \
-	    ${M4} ${M4FLAGS} -D__BASE_DIR=${BASEDIR} -D__FILE=$@ -D__TEMPLATE=${TEMPLATE} \
-	        -D__LANG=$$LANG \
+	    ${M4} ${M4FLAGS} -D__BASE_DIR=${BASEDIR} -D__FILE=$$BASE \
+	        -D__TEMPLATE=${TEMPLATE} -D__LANG=$$LANG \
 	        ${BASEDIR}/${TEMPLATE}.m4 \
-		| ${PERL} ${TOP}/mk/hstrip.pl > $@.$$LANG.prep; \
-	    echo '${DTD}' > $@.$$LANG.utf-8; \
-            ${XSLTPROC} --html --nonet --stringparam lang $$LANG ${XSL} \
-	        $@.$$LANG.prep >> $@.$$LANG.utf-8 2>/dev/null; \
-	    rm -f $@.$$LANG.prep; \
+		| ${HTML_STRIP} > $$OUT; \
+	    echo '${DTD}' > utf8/$$BASE.$$LANG; \
+            ${XSLTPROC} --html ${XSLTPROCFLAGS} --stringparam lang $$LANG ${XSL} \
+	        $$OUT 2>/dev/null \
+		| ${HTML_STRIP} >> utf8/$$BASE.$$LANG; \
+	    rm -f $$OUT; \
 	    case "$$LANG" in \
 	    en) \
-	        echo "URI: $@.$$LANG.utf-8" >> $@.var; \
-	        echo "Content-language: $$LANG" >> $@.var; \
-	        echo "Content-type: text/html;encoding=UTF-8" >> $@.var; \
-	        echo "" >> $@.var; \
-	        echo "URI: $@.$$LANG" >> $@.var; \
-	        echo "Content-language: $$LANG" >> $@.var; \
-	        echo "Content-type: text/html" >> $@.var; \
-	        echo "" >> $@.var; \
-	        cat $@.$$LANG.utf-8 | \
+	        echo "URI: utf8/$$BASE.$$LANG" >> $@; \
+	        echo "Content-language: $$LANG" >> $@; \
+	        echo "Content-type: text/html;encoding=UTF-8" >> $@; \
+	        echo "" >> $@; \
+	        echo "URI: $$BASE.$$LANG" >> $@; \
+	        echo "Content-language: $$LANG" >> $@; \
+	        echo "Content-type: text/html" >> $@; \
+	        echo "" >> $@; \
+	        cat utf8/$$BASE.$$LANG | \
 		    sed s/charset=UTF-8/charset=ISO-8859-1/ | \
 		    ${ICONV} -f UTF-8 -t ISO-8859-1 > \
-		    $@.$$LANG; \
+		    $$BASE.$$LANG; \
 	        ;; \
 	    ab|af|eu|ca|da|nl|fo|fr|fi|de|is|ga|it|no|nb|nn|pt|rm|gd|es|sv|sw) \
-	        echo "URI: $@.$$LANG.utf-8" >> $@.var; \
-	        echo "Content-language: $$LANG" >> $@.var; \
-	        echo "Content-type: text/html;encoding=UTF-8" >> $@.var; \
-	        echo "" >> $@.var; \
-	        echo "URI: $@.$$LANG.iso-8859-1" >> $@.var; \
-	        echo "Content-language: $$LANG" >> $@.var; \
-	        echo "Content-type: text/html;charset=ISO-8859-1" >> $@.var; \
-	        echo "" >> $@.var; \
-	        cat $@.$$LANG.utf-8 | \
+	        echo "URI: utf8/$$BASE.$$LANG" >> $@; \
+	        echo "Content-language: $$LANG" >> $@; \
+	        echo "Content-type: text/html;encoding=UTF-8" >> $@; \
+	        echo "" >> $@; \
+	        echo "URI: iso8859-1/$$BASE.$$LANG" >> $@; \
+	        echo "Content-language: $$LANG" >> $@; \
+	        echo "Content-type: text/html;charset=ISO-8859-1" >> $@; \
+	        echo "" >> $@; \
+	        cat utf8/$$BASE.$$LANG | \
 		    sed s/charset=UTF-8/charset=ISO-8859-1/ | \
 		    ${ICONV} -f UTF-8 -t ISO-8859-1 > \
-		    $@.$$LANG.iso-8859-1; \
-		cp -f $@.$$LANG.iso-8859-1 $@.$$LANG \
+		    iso8859-1/$$BASE.$$LANG; \
+		cp -f iso8859-1/$$BASE.$$LANG $$BASE.$$LANG; \
 	        ;; \
 	    *) \
 	        ;; \
 	    esac; \
-	    echo >> $@.var; \
+	    echo >> $@; \
 	done; \
 	rm -f ${BASEDIR}/base.htm; \
 	echo "."
 
 clean-www:
+	@echo -n "Clean:"
 	@for F in ${HTML}; do \
-		echo "rm -f $$F $$F.var"; \
-		rm -f $$F $$F.var; \
+		export BASE="`echo $$F | sed s/\.var//`"; \
+		echo -n " $$BASE"; \
+		rm -f $$F; \
 		for LANG in ${LANGUAGES}; do \
-			echo "rm -f $$F.$$LANG.* $$F.$$LANG"; \
-			rm -f $$F.$$LANG.* $$F.$$LANG; \
+			rm -f $$BASE.$$LANG {utf8,iso8859-1}/$$BASE.$$LANG; \
 		done; \
-	done
+	done;
+	@echo "."
 	@if [ "${CLEANFILES}" != "" ]; then \
 	    echo "rm -f ${CLEANFILES}"; \
 	    rm -f ${CLEANFILES}; \
 	fi
 
+install-www-makefile:
+	@export OUT=.Makefile.out; \
+	echo "# Generated by <build.www.mk> install on `date`" > $$OUT; \
+	echo "TOP=." >> $$OUT; \
+	echo "HTMLDIR=none" >> $$OUT; \
+	echo "BASEDIR=m4" >> $$OUT; \
+	echo "XSLDIR=xsl" >> $$OUT; \
+	echo "HTML=${HTML}" >> $$OUT; \
+	echo "CSS=${CSS}" >> $$OUT; \
+	echo "XSL=${XSL}" >> $$OUT; \
+	echo "XSLTPROC=${XSLTPROC}" >> $$OUT; \
+	echo "XSLTPROCFLAGS=${XSLTPROCFLAGS}" >> $$OUT; \
+	echo "M4=${M4}" >> $$OUT; \
+	echo "PERL=${PERL}" >> $$OUT; \
+	echo "ICONV=${ICONV}" >> $$OUT; \
+	echo "TEMPLATE=${TEMPLATE}" >> $$OUT; \
+	echo "CSS_TEMPLATE=${CSS_TEMPLATE}" >> $$OUT; \
+	echo "LANGUAGES=${LANGUAGES}" >> $$OUT; \
+	echo "CHARSETS=${CHARSETS}" >> $$OUT; \
+	echo "DEF_LANGUAGE=${DEF_LANGUAGE}" >> $$OUT; \
+	echo "include mk/build.www.mk" >> $$OUT; \
+	echo "${INSTALL_DATA} $$OUT ${HTMLDIR}/Makefile"; \
+	${SUDO} ${INSTALL_DATA} $$OUT ${DESTDIR}${HTMLDIR}/Makefile; \
+	rm -f $$OUT
+
+install-www-source:
+	@if [ -e "${DESTDIR}${HTMLDIR}/$$SRCFILE" \
+	      -a "${HTML_OVERWRITE}" = "" ]; then \
+		echo "${HTMLDIR}/$$SRCFILE exists; preserving"; \
+	else \
+		echo "${INSTALL_DATA} $$SRCFILE ${HTMLDIR}"; \
+		${SUDO} ${INSTALL_DATA} $$SRCFILE ${DESTDIR}${HTMLDIR}; \
+	fi
+	@if [ -e "${DESTDIR}${HTMLDIR}/Makefile"  \
+	      -a "${HTML_OVERWRITE}" = "" ]; then \
+		echo "${HTMLDIR}/Makefile exists; preserving"; \
+	else
+		${MAKE} install-www-makefile; \
+	fi
+
+install-www-base:
+	@if [ ! -d "${DESTDIR}${HTMLDIR}/mk" ]; then \
+		echo "${INSTALL_DATA_DIR} ${HTMLDIR}/mk"; \
+		${SUDO} ${INSTALL_DATA_DIR} ${DESTDIR}${HTMLDIR}/mk; \
+	fi
+	@for MK in ${MKDEPS}; do \
+		echo "${INSTALL_DATA} ${TOP}/mk/$$MK ${HTMLDIR}/mk"; \
+		${SUDO} ${INSTALL_DATA} ${TOP}/mk/$$MK ${DESTDIR}${HTMLDIR}/mk; \
+	done
+	@if [ ! -d "${DESTDIR}${HTMLDIR}/xsl" ]; then \
+		echo "${INSTALL_DATA_DIR} ${HTMLDIR}/xsl"; \
+		${SUDO} ${INSTALL_DATA_DIR} ${DESTDIR}${HTMLDIR}/xsl; \
+	fi
+	@for XSL in ${XSL}; do \
+		if [ -e "${DESTDIR}${HTMLDIR}/xsl/$$XSL" \
+		     -a "${HTML_OVERWRITE}" = "" ]; then \
+			echo "xsl/$$XSL: exists; preserving"; \
+		else \
+			echo "${INSTALL_DATA} $$XSL ${HTMLDIR}/xsl"; \
+			${SUDO} ${INSTALL_DATA} $$XSL ${DESTDIR}${HTMLDIR}/xsl; \
+		fi; \
+	done
+	@if [ ! -d "${DESTDIR}${HTMLDIR}/m4" ]; then \
+		echo "${INSTALL_DATA_DIR} ${HTMLDIR}/m4"; \
+		${SUDO} ${INSTALL_DATA_DIR} ${DESTDIR}${HTMLDIR}/m4; \
+	fi
+	@(cd ${BASEDIR}; for M4IN in `ls -1 *.m4`; do \
+		if [ -e "${DESTDIR}${HTMLDIR}/m4/$$M4IN" \
+		     -a "${HTML_OVERWRITE}" = "" ]; then \
+			echo "m4/$$M4IN: exists; preserving"; \
+		else \
+			echo "${INSTALL_DATA} $$M4IN ${HTMLDIR}/m4"; \
+			${SUDO} ${INSTALL_DATA} $$M4IN ${DESTDIR}${HTMLDIR}/m4; \
+		fi; \
+	done)
+
 install-www:
 	@if [ "${HTMLDIR}" = "none" ]; then \
 		exit 0; \
 	fi
+	@for CHARSET in ${CHARSETS}; do \
+		echo "${INSTALL_DATA_DIR} ${HTMLDIR}/$$CHARSET"; \
+		${SUDO} ${INSTALL_DATA_DIR} ${DESTDIR}${HTMLDIR}/$$CHARSET; \
+	done
 	@if [ ! -d "${DESTDIR}${HTMLDIR}" ]; then \
 		echo "${INSTALL_DATA_DIR} ${HTMLDIR}"; \
 		${SUDO} ${INSTALL_DATA_DIR} ${DESTDIR}${HTMLDIR}; \
 	fi
 	@if [ "${HTML_INSTSOURCE}" = "Yes" ]; then \
-		if [ ! -d "${DESTDIR}${HTMLDIR}/mk" ]; then \
-			echo "${INSTALL_DATA_DIR} ${HTMLDIR}/mk"; \
-			${SUDO} ${INSTALL_DATA_DIR} ${DESTDIR}${HTMLDIR}/mk; \
-		fi; \
-		for MK in ${MKDEPS}; do \
-			echo "${INSTALL_DATA} ${TOP}/mk/$$MK ${HTMLDIR}/mk"; \
-			${SUDO} ${INSTALL_DATA} ${TOP}/mk/$$MK \
-			    ${DESTDIR}${HTMLDIR}/mk; \
-		done; \
-       		if [ ! -d "${DESTDIR}${HTMLDIR}/xsl" ]; then \
-			echo "${INSTALL_DATA_DIR} ${HTMLDIR}/xsl"; \
-			${SUDO} ${INSTALL_DATA_DIR} ${DESTDIR}${HTMLDIR}/xsl; \
-		fi; \
-		for XSL in ${XSL}; do \
-			if [ -e "${DESTDIR}${HTMLDIR}/xsl/$$XSL" \
-			     -a "${HTML_OVERWRITE}" = "" ]; then \
-				echo "xsl/$$XSL: exists; preserving"; \
-			else \
-				echo "${INSTALL_DATA} $$XSL ${HTMLDIR}/xsl"; \
-				${SUDO} ${INSTALL_DATA} $$XSL \
-				    ${DESTDIR}${HTMLDIR}/xsl; \
-			fi; \
-		done; \
-		if [ ! -d "${DESTDIR}${HTMLDIR}/m4" ]; then \
-			echo "${INSTALL_DATA_DIR} ${HTMLDIR}/m4"; \
-			${SUDO} ${INSTALL_DATA_DIR} ${DESTDIR}${HTMLDIR}/m4; \
-		fi; \
-		(cd ${BASEDIR}; for M4IN in `ls -1 *.m4`; do \
-			if [ -e "${DESTDIR}${HTMLDIR}/m4/$$M4IN" \
-			     -a "${HTML_OVERWRITE}" = "" ]; then \
-				echo "m4/$$M4IN: exists; preserving"; \
-			else \
-				echo "${INSTALL_DATA} $$M4IN ${HTMLDIR}/m4"; \
-				${SUDO} ${INSTALL_DATA} $$M4IN \
-				    ${DESTDIR}${HTMLDIR}/m4; \
-			fi; \
-		done); \
+		${MAKE} install-www-base; \
 	fi
-	@for F in ${HTML_DEPS}; do \
+	@for F in ${HTML_EXTRA}; do \
 	    echo "${INSTALL_DATA} $$F ${HTMLDIR}"; \
 	    ${SUDO} ${INSTALL_DATA} $$F ${DESTDIR}${HTMLDIR}; \
 	done
 	@for F in ${HTML}; do \
-		rm -f $$F; \
-		if [ "${HTML_INSTSOURCE}" = "Yes" -a \
-		     ! -e "${DESTDIR}${HTMLDIR}/Makefile" ]; then \
-			echo "TOP=." > Makefile.prep; \
-			echo "HTML=${HTML}" >> Makefile.prep; \
-			echo "HTMLDIR=none" >> Makefile.prep; \
-			echo "M4=${M4}" >> Makefile.prep; \
-			echo "XSLTPROC=${XSLTPROC}" >> Makefile.prep; \
-			echo "PERL=${PERL}" >> Makefile.prep; \
-			echo "BASEDIR=m4" >> Makefile.prep; \
-			echo "TEMPLATE=${TEMPLATE}" >> Makefile.prep; \
-			echo "LANGUAGES=${LANGUAGES}" >> Makefile.prep; \
-			echo "XSL=xsl" >> Makefile.prep; \
-			echo "include mk/build.www.mk" >> Makefile.prep; \
-			echo "${INSTALL_DATA} Makefile.prep \
-			    ${HTMLDIR}/Makefile"; \
-			${SUDO} ${INSTALL_DATA} Makefile.prep \
-			    ${DESTDIR}${HTMLDIR}/Makefile; \
-			rm -f Makefile.prep; \
-		fi; \
+		export BASE="`echo $$F | sed s/\.var//`"; \
 		if [ "${HTML_INSTSOURCE}" = "Yes" ]; then \
-			export SF=`echo $$F |sed s,.html$$,.htm,`; \
-			if [ -e "${DESTDIR}${HTMLDIR}/$$SF" \
-			     -a "${HTML_OVERWRITE}" = "" ]; then \
-				echo "$$SF exists; preserving"; \
-			else \
-				echo "${INSTALL_DATA} $$SF ${HTMLDIR}"; \
-				${SUDO} ${INSTALL_DATA} $$SF \
-				    ${DESTDIR}${HTMLDIR}; \
-			fi; \
+			${MAKE} install-www-source \
+			    SRCFILE="`echo $$BASE |sed s,.html$$,.htm,`"
 		fi; \
-		if [ -e "${DESTDIR}${HTMLDIR}/$$F.var" \
+		if [ -e "${DESTDIR}${HTMLDIR}/$$F" \
 		     -a "${HTML_OVERWRITE}" = "" ]; then \
-			echo "$$F.var exists; preserving"; \
+			echo "$$F exists; preserving"; \
 		else \
-			echo "${INSTALL_DATA} $$F.var ${HTMLDIR}"; \
-			${SUDO} ${INSTALL_DATA} $$F.var ${DESTDIR}${HTMLDIR}; \
+			echo "${INSTALL_DATA} $$F ${HTMLDIR}"; \
+			${SUDO} ${INSTALL_DATA} $$F ${DESTDIR}${HTMLDIR}; \
 		fi; \
 		for LANG in ${LANGUAGES}; do \
-			for ENC in `ls -1 $$F.$$LANG*`; do \
-			    if [ -e "${DESTDIR}${HTMLDIR}/$$ENC" \
-			         -a "${HTML_OVERWRITE}" = "" ]; then \
-				    echo "$$ENC exists; preserving"; \
-			    else \
-				    echo "${INSTALL_DATA} $$ENC ${HTMLDIR}"; \
-				    ${SUDO} ${INSTALL_DATA} $$ENC \
-				        ${DESTDIR}${HTMLDIR}; \
-			    fi; \
+			if [ -e "${DESTDIR}${HTMLDIR}/$$BASE.$$LANG" \
+			     -a "${HTML_OVERWRITE}" = "" ]; then \
+				echo "$$BASE.$$LANG exists; preserving"; \
+			else \
+				echo "${INSTALL_DATA} $$BASE.$$LANG ${HTMLDIR}"; \
+				${SUDO} ${INSTALL_DATA} $$BASE.$$LANG ${DESTDIR}${HTMLDIR}; \
+			fi; \
+			for CHARSET in ${CHARSETS}; do \
+				if [ -e "$$CHARSET/$$BASE.$$LANG" ]; then \
+					if [ -e "${DESTDIR}${HTMLDIR}/$$CHARSET/$$BASE.$$LANG" \
+					     -a "${HTML_OVERWRITE}" = "" ]; then \
+						echo "$$CHARSET/$$BASE.$$LANG exists; preserving"; \
+					else \
+						echo "${INSTALL_DATA} $$CHARSET/$$BASE.$$LANG ${HTMLDIR}/$$CHARSET"; \
+						${SUDO} ${INSTALL_DATA} $$CHARSET/$$BASE.$$LANG ${DESTDIR}${HTMLDIR}/$$CHARSET; \
+					fi; \
+				fi; \
 			done; \
 		done; \
 	done
 
-.PHONY: install deinstall clean cleandir regress depend
-.PHONY: install-www clean-www
+.PHONY: install deinstall clean cleandir regress depend clean-www
+.PHONY: install-www install-www-makefile install-www-source install-www-base
 
 include ${TOP}/mk/build.common.mk
 include ${TOP}/mk/build.subdir.mk

7cf4a66639c89347362875697645ba27563b6f31
Julien Nadeau Carriere <vedge@hypertriton.com>
2019-12-24 11:50:13 +0000

    pull in `agar-rg-config --libs` and `agar-rg-config --cflags`
    git-svn-id: file:///home/dev/bsdbuild/trunk@1197 086a213a-9b02-0410-a1af-a7f5267bcc5c

--- BSDBuild/agar-map.pm
+++ BSDBuild/agar-map.pm
@@ -25,11 +25,13 @@ sub TEST_agar_map
 		MkPrintSN('checking whether Agar-MAP works...');
 		MkExecOutputPfx($pfx, 'agar-config', '--cflags', 'AGAR_CFLAGS');
 		MkExecOutputPfx($pfx, 'agar-config', '--libs', 'AGAR_LIBS');
+		MkExecOutputPfx($pfx, 'agar-rg-config', '--cflags', 'AGAR_RG_CFLAGS');
+		MkExecOutputPfx($pfx, 'agar-rg-config', '--libs', 'AGAR_RG_LIBS');
 		MkExecOutputPfx($pfx, 'agar-map-config', '--cflags', 'AGAR_MAP_CFLAGS');
 		MkExecOutputPfx($pfx, 'agar-map-config', '--libs', 'AGAR_MAP_LIBS');
 		MkCompileC('HAVE_AGAR_MAP',
-		           '${AGAR_MAP_CFLAGS} ${AGAR_CFLAGS}',
-		           '${AGAR_MAP_LIBS} ${AGAR_LIBS}',
+		           '${AGAR_MAP_CFLAGS} ${AGAR_RG_CFLAGS} ${AGAR_CFLAGS}',
+		           '${AGAR_MAP_LIBS} ${AGAR_RG_LIBS} ${AGAR_LIBS}',
 		           $testCode);
 		MkSaveIfTrue('${HAVE_AGAR_MAP}', 'AGAR_MAP_CFLAGS', 'AGAR_MAP_LIBS');
 	MkElse;
@@ -66,8 +68,8 @@ BEGIN
 	$TESTS{$n}   = \&TEST_agar_map;
 	$DISABLE{$n} = \&DISABLE_agar_map;
 	$EMUL{$n}    = \&EMUL_agar_map;
-	$DEPS{$n}    = 'cc,agar-rg,agar-math,agar';
+	$DEPS{$n}    = 'cc,agar,agar-rg';
 
-	@{$EMULDEPS{$n}} = qw(agar);
+#	@{$EMULDEPS{$n}} = qw(agar);
 }
 ;1

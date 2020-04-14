90512c19956e4b4da643cf59cd3ad7843b41c7bb
Julien Nadeau Carriere <vedge@hypertriton.com>
2020-04-14 01:32:24 +0000

    Remove freesg, freesg_sg (now agar-sg). ag_rg was merged into ag_map.
    git-svn-id: file:///home/dev/bsdbuild/trunk@1218 086a213a-9b02-0410-a1af-a7f5267bcc5c

--- BSDBuild/agar-map.pm
+++ BSDBuild/agar-map.pm
@@ -25,14 +25,13 @@ sub TEST_agar_map
 		MkPrintSN('checking whether Agar-MAP works...');
 		MkExecOutputPfx($pfx, 'agar-config', '--cflags', 'AGAR_CFLAGS');
 		MkExecOutputPfx($pfx, 'agar-config', '--libs', 'AGAR_LIBS');
-		MkExecOutputPfx($pfx, 'agar-rg-config', '--cflags', 'AGAR_RG_CFLAGS');
-		MkExecOutputPfx($pfx, 'agar-rg-config', '--libs', 'AGAR_RG_LIBS');
 		MkExecOutputPfx($pfx, 'agar-map-config', '--cflags', 'AGAR_MAP_CFLAGS');
 		MkExecOutputPfx($pfx, 'agar-map-config', '--libs', 'AGAR_MAP_LIBS');
-		MkCompileC('HAVE_AGAR_MAP',
-		           '${AGAR_MAP_CFLAGS} ${AGAR_RG_CFLAGS} ${AGAR_CFLAGS}',
-		           '${AGAR_MAP_LIBS} ${AGAR_RG_LIBS} ${AGAR_LIBS}',
-		           $testCode);
+
+		MkCompileC('HAVE_AGAR_MAP', '${AGAR_MAP_CFLAGS} ${AGAR_CFLAGS}',
+		                            '${AGAR_MAP_LIBS} ${AGAR_LIBS}',
+		                            $testCode);
+
 		MkSaveIfTrue('${HAVE_AGAR_MAP}', 'AGAR_MAP_CFLAGS', 'AGAR_MAP_LIBS');
 	MkElse;
 		DISABLE_agar_map();
@@ -68,7 +67,7 @@ BEGIN
 	$TESTS{$n}   = \&TEST_agar_map;
 	$DISABLE{$n} = \&DISABLE_agar_map;
 	$EMUL{$n}    = \&EMUL_agar_map;
-	$DEPS{$n}    = 'cc,agar,agar-rg';
+	$DEPS{$n}    = 'cc,agar';
 
 #	@{$EMULDEPS{$n}} = qw(agar);
 }

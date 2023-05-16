09d37923f2a961cd5041ff212afac47d18b72803
Julien Nadeau Carriere <vedge@hypertriton.com>
2023-05-16 14:48:14 +0000

    Include `SDL.h` instead of `SDL2/SDL.h`. Thanks Wally!
    git-svn-id: file:///home/dev/bsdbuild/trunk@1270 086a213a-9b02-0410-a1af-a7f5267bcc5c

--- BSDBuild/sdl2.pm
+++ BSDBuild/sdl2.pm
@@ -2,12 +2,12 @@
 
 my $testCode = << 'EOF';
 #ifdef _USE_SDL_FRAMEWORK
-# include <SDL2/SDL.h>
+# include <SDL.h>
 # ifdef main
 #  undef main
 # endif
 #else
-# include <SDL2/SDL.h>
+# include <SDL.h>
 #endif
 int main(int argc, char *argv[]) {
 	SDL_Surface *su;

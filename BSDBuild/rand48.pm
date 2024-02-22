4c80296e9790e5b16b45d168edeea4100cd8b32e
Julien Nadeau Carriere <vedge@hypertriton.com>
2024-02-22 06:41:07 +0000

    `rand48`: Add cmake support.
    `agar`: Also define CFLAGS and LIBS for NET, SG, SK and VG.
    
    
    
    git-svn-id: file:///home/dev/bsdbuild/trunk@1278 086a213a-9b02-0410-a1af-a7f5267bcc5c

--- BSDBuild/rand48.pm
+++ BSDBuild/rand48.pm
@@ -1,9 +1,8 @@
 # Public domain
 
-sub TEST_rand48
-{
-	TryCompile 'HAVE_RAND48', << 'EOF';
+my $testCode = << 'EOF';
 #include <stdlib.h>
+
 int
 main(int argc, char *argv[])
 {
@@ -18,6 +17,27 @@ main(int argc, char *argv[])
 	return (0);
 }
 EOF
+
+sub CMAKE_rand48
+{
+	my $code = MkCodeCMAKE($testCode);
+
+	return << "EOF";
+macro(Check_Rand48)
+	check_c_source_compiles("
+$code" HAVE_RAND48)
+	if (HAVE_RAND48)
+		BB_Save_Define(HAVE_RAND48)
+	else()
+		BB_Save_Undef(HAVE_RAND48)
+	endif()
+endmacro()
+EOF
+}
+
+sub TEST_rand48
+{
+	TryCompile('HAVE_RAND48', $testCode);
 }
 
 sub DISABLE_rand48
@@ -32,6 +52,7 @@ BEGIN
 
 	$DESCR{$n}   = 'the rand48(3) family of functions';
 	$TESTS{$n}   = \&TEST_rand48;
+	$CMAKE{$n}   = \&CMAKE_rand48;
 	$DISABLE{$n} = \&DISABLE_rand48;
 	$DEPS{$n}    = 'cc';
 }

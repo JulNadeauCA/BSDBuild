ba5f68c4759b51cbbc072ec49b98340c2e65774c
Julien Nadeau Carriere <vedge@hypertriton.com>
2024-02-29 05:37:36 +0000

    Check for libidn2.
    git-svn-id: file:///home/dev/bsdbuild/trunk@1281 086a213a-9b02-0410-a1af-a7f5267bcc5c

--- BSDBuild/libidn.pm
+++ BSDBuild/libidn.pm
@@ -4,14 +4,13 @@ my $testCode = << 'EOF';
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stringprep.h>
-#include <idna.h>
+#include <idn2.h>
 
 int main(int argc, char *argv[]) {
 	char *buf = "foo.com", *p;
 	int rv;
-	rv = idna_to_unicode_lzlz(buf, &p, 0);
-	return ((rv == IDNA_SUCCESS) ? 0 : 1);
+	rv = idn2_to_unicode_lzlz(buf, &p, 0);
+	return ((rv == IDN2_OK) ? 0 : 1);
 }
 EOF
 
@@ -19,11 +18,11 @@ sub TEST_libidn
 {
 	my ($ver, $pfx) = @_;
 	
-	MkExecPkgConfig($pfx, 'libidn', '--modversion', 'LIBIDN_VERSION');
-	MkExecPkgConfig($pfx, 'libidn', '--cflags', 'LIBIDN_CFLAGS');
-	MkExecPkgConfig($pfx, 'libidn', '--libs', 'LIBIDN_LIBS');
+	MkExecPkgConfig($pfx, 'libidn2', '--modversion', 'LIBIDN_VERSION');
+	MkExecPkgConfig($pfx, 'libidn2', '--cflags', 'LIBIDN_CFLAGS');
+	MkExecPkgConfig($pfx, 'libidn2', '--libs', 'LIBIDN_LIBS');
 	MkIfFound($pfx, $ver, 'LIBIDN_VERSION');
-		MkPrintSN('checking whether libidn works...');
+		MkPrintSN('checking whether libidn2 works...');
 		MkCompileC('HAVE_LIBIDN', '${LIBIDN_CFLAGS}', '${LIBIDN_LIBS}', $testCode);
 		MkIfFalse('${HAVE_LIBIDN}');
 			MkDisableFailed('libidn');
@@ -45,7 +44,7 @@ BEGIN
 {
 	my $n = 'libidn';
 
-	$DESCR{$n}   = 'libidn';
+	$DESCR{$n}   = 'libidn2';
 	$URL{$n}     = 'http://www.gnu.org/software/libidn';
 	$TESTS{$n}   = \&TEST_libidn;
 	$DISABLE{$n} = \&DISABLE_libidn;
